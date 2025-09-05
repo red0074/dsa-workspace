988f70f4cadeb49750ca394ead34bd0de9a95442
dinesh <pdinesh122003@gmail.com>
2025-09-05 18:14:11 +0530

    circular queue using array

---
+++ strivers/day5/collections/queues/CircularQueueArray.java
@@ -0,0 +1,61 @@
+package day5.collections.queues;
+
+public class CircularQueueArray {
+    static class Queue{
+        static int arr[];
+        static int size;
+        static int rear=-1;
+        static int front=-1;
+        Queue(int size){
+            arr=new int[size];
+            this.size=size;
+        }
+        public static boolean isEmpty(){
+            return rear==-1 && front==-1;
+        }
+        public static boolean isFull(){
+            return (rear+1)%size==front;
+        }
+        public static void add(int data){
+            if(isFull()){
+                System.out.println("full queue");
+                return;
+            }
+            if(front==-1){
+                front=0;
+            }
+            rear=(rear+1)%size;
+            arr[rear]=data;
+        }
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("Empty queue");
+                return -1;
+            }
+            int res=arr[front];
+            if(rear==front){
+                rear=front=-1;
+            }else{
+                front=(front+1)%size;
+            }
+            return res;
+        }
+        public static int peek(){
+            if(isEmpty()){
+                System.out.println("empty queue");
+                return -1;
+            }
+            return arr[front];
+        }
+    }
+    public static void main(String args[]){
+        Queue q=new Queue(5);
+        q.add(10);
+        q.add(11);
+        q.add(12);
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
